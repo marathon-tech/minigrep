99b3bb674947a925a44944802a4dda314aa3f541
fujia <fujia.site@outlook.com>
2023-06-14 07:57:39 +0800

    refactor(structure): refine organization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use mini_grep::Config;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -14,36 +14,10 @@ fn main() {
     process::exit(1);
   });
 
-  if let Err(e) = run(config) {
+  if let Err(e) = mini_grep::run(config) {
     println!("Application error: {e}");
 
     process::exit(1);
-  }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  let contents = fs::read_to_string(config.file_path)?;
-
-  println!("with text:\n{contents}");
-
-  Ok(())
-}
-
-struct Config {
-  query: String,
-  file_path: String,
-}
-
-impl Config {
-  fn build(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
-    }
-
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Ok(Config { query, file_path })
   }
 }
 
