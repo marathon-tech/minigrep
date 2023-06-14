21ac0ffe774e82d804a4a1f4047f55cf3211ebfd
fujia <fujia.site@outlook.com>
2023-06-14 18:15:23 +0800

    feat: finish basic features

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ fn main() {
   // println!("searching for {}", query);
   // println!("In file {}", file_path);
   let config = Config::build(&args).unwrap_or_else(|err| {
-    println!("Problem parsing arguments: {err}");
+    // println!("Problem parsing arguments: {err}");
+    eprintln!("Problem parsing arguments: {err}");
     process::exit(1);
   });
 
   if let Err(e) = mini_grep::run(config) {
-    println!("Application error: {e}");
+    // println!("Application error: {e}");
+    eprintln!("Application error: {e}");
 
     process::exit(1);
   }
